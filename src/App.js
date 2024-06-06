e9cb6e25f0dda9ddaba16b697a26c16131339437
Dharanesh Reddy <dharan35849@gmail.com>
2024-06-05 22:19:35 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Quiz from './components/Quiz';
+import QuizResult from './components/QuizResult';
+
+function App() {
+  return (
+    <div>
+         <Quiz/>
+    </div>
+  );
+}
+
+export default App;
